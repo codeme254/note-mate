a06dbc712f4ecb20af4515cbfc3f747dc83f7724
codeme254 <otwomadennis365@gmail.com>
2023-06-09 16:29:14 +0300

    create the about section

--- notemate-frontend/src/App.jsx
+++ notemate-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import LandingNav from './components/LandingNav/LandingNav';
 import LandingHeroMain from './components/LandingHero/LandingHeroMain';
 import Features from "./components/Features/Features";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="app">
       <LandingNav />
       <LandingHeroMain />
+      <About />
       <Features />
     </div>
   );
