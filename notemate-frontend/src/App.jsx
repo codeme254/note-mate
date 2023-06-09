ccdc5ed4d1b4c53535e2f935507d0f0b8e14b358
codeme254 <otwomadennis365@gmail.com>
2023-06-09 15:58:01 +0300

    add the features section

--- notemate-frontend/src/App.jsx
+++ notemate-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import LandingNav from './components/LandingNav/LandingNav';
 import LandingHeroMain from './components/LandingHero/LandingHeroMain';
+import Features from "./components/Features/Features";
 
 function App() {
   return (
     <div className="app">
       <LandingNav />
       <LandingHeroMain />
+      <Features />
     </div>
   );
 }
