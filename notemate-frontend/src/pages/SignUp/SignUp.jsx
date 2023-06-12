6d23927508ee6d93e48e2e50520b760143fc2e55
codeme254 <otwomadennis365@gmail.com>
2023-06-12 16:33:11 +0300

    add sign up form

--- notemate-frontend/src/pages/SignUp/SignUp.jsx
+++ notemate-frontend/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,41 @@
+import { Link } from 'react-router-dom';
+import "./SignUp.css";
 const SignUp = () => {
     return(
-        <div>
-            <h1>sign up</h1>
+        <div className='sign-up-container'>
+            <h2 className='form-page-title'>Create your notemate account</h2>
+            <form action="" className='sign-up-form'>
+                <div className="form-group">
+                    <label htmlFor="firstName" className="form-group-lable">First Name</label>
+                    <input type="text" className="form-text-input" placeholder="first name" id="firstName" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="lastName" className="form-group-lable">last Name</label>
+                    <input type="text" className="form-text-input" placeholder="last name" id="lastName" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="emailAddress" className="form-group-lable">email address</label>
+                    <input type="email" className="form-text-input" placeholder="email address" id="emailAddress" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="username" className="form-group-lable">pick a username</label>
+                    <input type="text" className="form-text-input" placeholder="username" id="username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password" className="form-group-lable">create a strong password</label>
+                    <input type="password" className="form-text-input" placeholder="password" id="password" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confpass" className="form-group-lable">confirm your password</label>
+                    <input type="password" className="form-text-input" placeholder="confirm your password" id="confpass" />
+                </div>
+                <div className="form-bottom-controls">
+                <button type="submit" className='form-btn'>Create account</button>
+
+<p className='form-guide'>Already have an account? <Link to="/login">login</Link> </p>
+<Link to="/" className='form-back'>Go back home</Link>
+                </div>
+            </form>
         </div>
     )
 }
