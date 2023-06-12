ad30cfae57684c700cc5f4b054820854ce1c0544
codeme254 <otwomadennis365@gmail.com>
2023-06-12 15:22:44 +0300

    make landing page responsive

--- notemate-frontend/src/components/LandingNav/LandingNav.jsx
+++ notemate-frontend/src/components/LandingNav/LandingNav.jsx
@@ -1,9 +1,24 @@
 import "./LandingNav.css";
+import {HiOutlineMenuAlt1} from 'react-icons/hi';
+import {AiOutlineCloseCircle} from 'react-icons/ai';
+import { useState, useRef } from "react";
 const LandingNav = () => {
+  const navRef = useRef(null);
+  const [navIsActive, setNavIsActive] = useState(false);
+  const [icon, setIcon] = useState(null)
+  
+  const handleToggleNav = () => {
+    setNavIsActive(!navIsActive)
+    navIsActive ? navRef.current.classList.remove("landing-nav-abs") : navRef.current.classList.add("landing-nav-abs");
+    navIsActive ? setIcon(AiOutlineCloseCircle) : setIcon(HiOutlineMenuAlt1)
+  }
   return (
     <header className="landing-nav__header">
       <h2 className="logo">Notemate</h2>
-      <nav className="landing-nav__nav">
+      <button className="menu-icon" onClick={handleToggleNav}>
+        {icon}
+      </button>
+      <nav className="landing-nav__nav" ref={navRef}>
         <ol className="landing-nav__nav-list">
           <li className="landing-nav__nav-list--item">
             <a href="">home</a>
