ca9f5feb4e2940eaa506e3f957974fe3f4d8980d
codeme254 <otwomadennis365@gmail.com>
2023-06-15 16:42:39 +0300

    get all notes, get specific note and create a note

--- notemate-backend/controllers/notesController.js
+++ notemate-backend/controllers/notesController.js
@@ -1,50 +1,83 @@
-import sql from 'mssql';
-import { config } from '../db/config.js';
-const pool = new sql.ConnectionPool(config.sql)
-await pool.connect()
+import sql from "mssql";
+import { config } from "../db/config.js";
+const pool = new sql.ConnectionPool(config.sql);
+await pool.connect();
 
-const checkIfUserExists = async username => {
-    const user = await pool.request()
+const checkIfUserExists = async (username) => {
+  const user = await pool
+    .request()
     .input("username", sql.VarChar, username)
-    .query("SELECT * FROM users WHERE username = @username")
+    .query("SELECT * FROM users WHERE username = @username");
 
-    if (user.recordset.length === 0) return false
-    return true
-}
+  if (user.recordset.length === 0) return false;
+  return true;
+};
 // console.log(await checkIfUserExists("gracebaker"))
 // console.log(await checkIfUserExists("sdlfksdlfjdlskjfdl"))
 
 export const getNotes = async (req, res) => {
   try {
     const notes = await pool.request().query("SELECT * FROM notes");
-    if (notes.recordset.length == 0){
-        res.status(204).json({ message: "No notes were found at this time" })
+    if (notes.recordset.length == 0) {
+      res.status(204).json({ message: "No notes were found at this time" });
     } else {
-        res.status(200).json(notes.recordset)
+      res.status(200).json(notes.recordset);
     }
-  } catch (error){
-    res.status(400).json({ message: "An error was encountered while attempting this operation" })
+  } catch (error) {
+    res
+      .status(400)
+      .json({
+        message: "An error was encountered while attempting this operation",
+      });
   }
 };
 
 export const getNote = async (req, res) => {
   let { noteTitle } = req.params;
   try {
-    const note = await pool.request()
-    .input("noteTitle", sql.VarChar, noteTitle)
-    .query("SELECT * FROM notes WHERE title = @noteTitle")
-    if (note.recordset.length === 0){
-        res.status(204).json({ message: "We didn't find any note(s) with that title"})
+    const note = await pool
+      .request()
+      .input("noteTitle", sql.VarChar, noteTitle)
+      .query("SELECT * FROM notes WHERE title = @noteTitle");
+    if (note.recordset.length === 0) {
+      res
+        .status(204)
+        .json({ message: "We didn't find any note(s) with that title" });
     } else {
-        res.status(200).json(note)
+      res.status(200).json(note);
     }
-  } catch (error){
-    res.status(400).json({ message: "An error was encountered while trying to retrieve that note"})
+  } catch (error) {
+    res
+      .status(400)
+      .json({
+        message: "An error was encountered while trying to retrieve that note",
+      });
   }
 };
 
-export const createNote = (req, res) => {
-  res.send("Create a note using this route");
+export const createNote = async (req, res) => {
+  const { username } = req.params;
+  const userExists = await checkIfUserExists(username);
+  if (userExists) {
+    try {
+      const { title, synopsis, body } = req.body;
+      const newNote = await pool
+        .request()
+        .input("username", sql.VarChar, username)
+        .input("title", sql.VarChar, title)
+        .input("synopsis", sql.VarChar, synopsis)
+        .input("body", sql.VarChar, body)
+        .query(
+          "INSERT INTO notes (username, title, synopsis, body) VALUES (@username, @title, @synopsis, @body)"
+        );
+
+      res.status(200).json({ message: "New note created successfuly" });
+    } catch (error) {
+      res.status(400).json({ message: `${error.message}` });
+    }
+  } else {
+    res.status(403).json({ message: `${username} does not exist` });
+  }
 };
 
 export const updateNote = (req, res) => {
