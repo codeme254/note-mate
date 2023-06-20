e3a0c9cb1b03582844e0bdc7bb9849d9ad9dfa90
codeme254 <otwomadennis365@gmail.com>
2023-06-20 16:28:41 +0300

    implement convert notes to pdf

--- notemate-frontend/src/components/NotesPreview/NotesPreview.jsx
+++ notemate-frontend/src/components/NotesPreview/NotesPreview.jsx
@@ -1,5 +1,8 @@
-import { AiOutlineSave, AiOutlineLike, AiOutlineFilePdf } from "react-icons/ai";
+import { AiOutlineSave, AiOutlineLike, AiOutlineFilePdf, AiOutlineDownload } from "react-icons/ai";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import "./NotesPreview.css";
+import NotesPDF from "./NotesPDF";
+
 const NotesPreview = ({
   firstName,
   lastName,
@@ -8,39 +11,61 @@ const NotesPreview = ({
   bodyPreview,
   username,
   datePosted,
-  initials,
 }) => {
   return (
-    <div className="notes-preview">
-      <div className="notes-preview__avartar">
-        {firstName && lastName && `${firstName.charAt(0)}${lastName.charAt(0)}`}
-      </div>
-      <div className="notes-preview__main">
-        <h5 className="notes-preview__name">
-          {firstName} {lastName}
-        </h5>
-        <p className="notes-preview__author">
-          By: {username} on {datePosted}
-        </p>
-        <h3 className="notes-preview__title">{title}</h3>
-        <p className="notes-preview__synopsis">{synposis}</p>
-        <p className="notes-preview__body-preview">{bodyPreview}</p>
-        <div className="notes-controls">
-          <button className="notes-controls__control">
-            <AiOutlineLike />
-            <p>clap</p>
-          </button>
-          <button className="notes-controls__control">
-            <AiOutlineFilePdf />
-            <p>Download as pdf</p>
-          </button>
-          <button className="notes-controls__control">
-            <AiOutlineSave />
-            <p>save to favorites</p>
-          </button>
+    <>
+      <div className="notes-preview">
+        <div className="notes-preview__avartar">
+          {firstName &&
+            lastName &&
+            `${firstName.charAt(0)}${lastName.charAt(0)}`}
+        </div>
+        <div className="notes-preview__main">
+          <h5 className="notes-preview__name">
+            {firstName} {lastName}
+          </h5>
+          <p className="notes-preview__author">
+            By: {username} on {datePosted}
+          </p>
+          <h3 className="notes-preview__title">{title}</h3>
+          <p className="notes-preview__synopsis">{synposis}</p>
+          <p className="notes-preview__body-preview">{bodyPreview}</p>
+          <div className="notes-controls">
+            <button className="notes-controls__control">
+              <AiOutlineLike />
+              <p>clap</p>
+            </button>
+
+            <button
+              className="notes-controls__control"
+              title="download notes as pdf"
+            >
+              <PDFDownloadLink
+                document={
+                  <NotesPDF
+                    title={title}
+                    synposis={synposis}
+                    bodyPreview={bodyPreview}
+                  />
+                }
+                fileName={title}
+              >
+                <AiOutlineFilePdf />
+                <AiOutlineDownload />
+                {({loading}) => (loading ? <p>Loading document...</p> : <p>Download as pdf</p>)}
+              </PDFDownloadLink>
+              {/* <p>Download as pdf</p> */}
+            </button>
+
+            <button className="notes-controls__control">
+              <AiOutlineSave />
+              <p>save to favorites</p>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {/* <NotesPDF title={title} synposis={synposis} bodyPreview={bodyPreview} /> */}
+    </>
   );
 };
 export default NotesPreview;
